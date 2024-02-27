ece744c7680db9cba5e606d2e9f66937f6303f00
Achmad Dhikrillah <login-swift-achmad.dhikrillah@gojek.com>
2024-02-27 19:55:12 +0700

    add cors config js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async headers() {
@@ -13,6 +14,6 @@ const nextConfig = {
             }
         ]
     }
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
