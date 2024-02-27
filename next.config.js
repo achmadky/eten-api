b863d08068f0b72fc912ae15436e1d4c7c7e2173
Achmad Dhikrillah <login-swift-achmad.dhikrillah@gojek.com>
2024-02-27 23:41:13 +0700

    add middleware ts and other config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,33 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     async headers() {
       return [
         {
-          // matching all API routes
-          source: "/api/:path*",
+          // Routes this applies to
+          source: "/api/(.*)",
+          // Headers
           headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
-        }
-      ]
-    }
+            // Allow for specific domains to have access or * for all
+            {
+              key: "Access-Control-Allow-Origin",
+              value: "*",
+              // DOES NOT WORK
+              // value: process.env.ALLOWED_ORIGIN,
+            },
+            // Allows for specific methods accepted
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET, POST, PUT, DELETE, OPTIONS",
+            },
+            // Allows for specific headers accepted (These are a few standard ones)
+            {
+              key: "Access-Control-Allow-Headers",
+              value: "Content-Type, Authorization",
+            },
+          ],
+        },
+      ];
+    },
   };
   
+  module.exports = nextConfig;
