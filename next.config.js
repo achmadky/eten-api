4d6d669f8c84513ffdc132006ce4028ed3846295
Achmad Dhikrillah <login-swift-achmad.dhikrillah@gojek.com>
2024-02-27 23:51:22 +0700

    last try config cors

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
         return [
           {
             source: '/api/:path*',
-            destination: 'https://api.example.com/:path*',
+            destination: 'https://eten-api.vercel.app/pages/path*',
           },
         ]
       },
